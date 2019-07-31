bde36c230f20bcbc6b1cb8938a82a8010d5875a6
viktoria <vik.slepchenko@gmail.com>
2019-07-31 18:14:11 +0200

    asd

--- types.js
+++ types.js
@@ -1,39 +1,108 @@
-console.log("welcome");
-var Item = /** @class */ (function () {
-    function Item(key, value) {
-        this.key = key;
+// console.log("welcome");
+// let line = "circle,solar,dinner,floor,line,claws,thumb,milk,afraid,lie,same,cabin,market,yard,everybody,invented,gravity,ask,find,ate,down,tiny,whispered,try,empty,main,refer,then,accident,post,world,something,close,yes,title,search,engine,support,steel,according,finish,crack,speed,trade,taste,think,somehow,coming,dirty,feet,alphabet,orange,importance,wrong,trouble,use,furniture,teacher,tape,student,quick,organization,triangle,eleven,send,song,baseball,doctor,spend,news,open,rice,hunter,north,team,capital,examine,mistake,third,chicken,court,halfway,pound,element,equipment,six,circus,round,understanding,forget,probably,purpose,join,friendly,cloth,leg,written,leave,begun,bean,danger,which,force,sides,spent,choice,herself,closer,stick,meant,heart,food,composition,heat,city,label,child,failed,foreign,donkey,cake,potatoes,brain,certain,handsome,snow,specific,within,aboard,ear,represent,jet,prepare,grow,because,smallest,slave,factor,again,sad,paper,directly,now,wherever,tip,shore,proper,yellow,month,goose,me,fallen,television,water,leader,stretch,individual,farmer,family,struggle,machinery,word,say,naturally,it,enough,sang,brother,get,out,planet,feathers,cloud,divide,beneath,gone,remember,garage,possible,proud,guide,flight,fair,earlier,eat,repeat,army,since,mathematics,forward,secret,way,excitement,scared,brush,worried,possibly,slide,ocean,forgotten,scientific,unhappy,right,speech,event,face,own,satellites,upper,shadow,parallel,parts,present,connected,plus,period,manner,golden,either,near,chamber,original,wing,plates,done,toy,strong,does,seen,better,spoken,hungry,were,person,tree,passage,felt,joined,there,roll,completely,important,grass,older,teeth,room,bee,underline,birds,desk,locate,pony,spread,impossible,indicate,question,science,body,hundred,particular,spirit,missing,value,castle,prove,position,flat,hospital,one,among,imagine,community,rabbit,lion,gas,principle,nearby,nature,hat,lovely,lay,mainly,burst,put,cream,flame,has,log,cut,count,cowboy,run,farther,wooden,remain,thread,biggest,fruit,ride,loss,anything,discover,noon,lungs,that,several,nobody,jungle,end,troops,dead,successful,arrive,accept,nails,pupil,electricity,young,putting,chose,swung,chemical,shop,avoid,some,classroom,raw,pair,writing,smaller,space,environment,least,believed,hollow,dig,strength,waste,suggest,beat,chest,stock,changing,shot,crop,rising,sail,exercise,simple,industry,cook,happened,week,route,weak,species,are,safety,without,instrument,cookies,numeral,until,extra,star,appropriate,depth,manufacturing,discuss,sea,wrapped,total,sound,half,cold,reach,final,found,bag,animal,harbor,likely,adventure,piece,agree,they,train,onto,expression,thin,sign,note,dawn,sit,supper,rest,night,fairly,journey,walk,tent,labor,easy,bare,quietly,us,ice,fell,arrange,ordinary,fourth,fifth,softly,fifteen,harder,knife,want,roof,difference,sort,getting,band,large,its,about,generally,pain,jump,do,task,draw,perfect,screen,recognize,mice,base,develop,ill,improve,came,state,sunlight,obtain,real,tightly,orbit,brief,slow,thick,swimming,storm,sell,division,where,substance,color,visit,weigh,skin,most,little,signal,thirty,clothes,bear,mind,happy,needle,past,using,mighty,system,basic,particles,firm,type,egg,garden,ability,work,liquid,fear,conversation,worse,yourself,sentence,hair,elephant,forty,fighting,occasionally,blanket,lying,volume,automobile,vertical,dark,seems,pond,port,brick,oxygen,drop,clothing,straw,chair,process,fence,score,cup,evening,law,pour,rod,frequently,careful,so,nearly,wind,comfortable,block,tobacco,men,necessary,press,scene,deeply,grandmother,not,knowledge,century,lips,list"+
+// "adult,soil,task,parts,exchange,birds,plane,off,bare,rush,stream,means,perfectly,depend,create,possibly,bit,result,open,gravity,bill,more,surrounded,position,avoid,bet,remarkable,know,chief,shirt,labor,blank,contrast,supply,trouble,community,sale,seat,mother,help,problem,safe,unit,adult,stone,myself,food,wooden,hand,pleasant,operation,pure,loss,visit,settlers,horse,course,none,completely,equipment,particularly,thought,accident,pride,cold,social,situation,visitor,opportunity,remarkable,missing,first,pattern,moving,ought,blind,ten,someone,planning,table,section,close,ago,nothing,shelter,apple,continued,picture,understanding,successful,long,composed,sudden,list,throw,heart,lungs,motor,five,popular,share,sometime,old,pictured,rather,garage,with,pay,each,cotton,supper,setting,wire,stone,pink,seldom,stop,crowd,aside,never,window,imagine,wet,shop,got,truth,different,dream,wool,badly,sets,highest,struggle,force,worker,last,stay,needs,coffee,dried,variety,ruler,toy,wherever,over,swing,transportation,board,fix,low,stop,swim,fought,sound,consist,death,can,gasoline,hunt,where,speed,horn,army,equator,depth,pupil,only,deer,oldest,smallest,occasionally,failed,essential,town,become,closely,between,belong,taught,plane,goose,gate,drop,jungle,now,stand,bridge,means,shore,length,browserling,refused,somebody,tide,citizen,noun,being,pictured,running,movement,ate,twice,through,behavior,square,face,black,birthday,nobody,southern,bush,angry,shade,slight,fellow,smile,bag,area,spell,living,second,twice,smallest,characteristic,careful,trip,forest,loss,string,wait,quick,softly,from,duty,thus,shop,cat,large,softly,needle,dish,gold,truck,parallel,minerals,no,new,dig,naturally,prize,joy,angle,soldier,doing,small,heard,earn,port,board,free,grew,highest,key,product,poor,standard,mountain,garage,slave,handsome,fuel,wheel,principle,extra,score,angle,thick,interest,audience,three,camp,raise,contain,particularly,season,born,spite,visit,officer,blood,cannot,recent,shade,hello,stretch,select,track,at,finally,silk,window,certain,express,low,forgotten,its,held,blew,electricity,acres,carried,straw,clothes,exactly,forget,mirror,length,press,might,leg,swimming,rich,end,alike,cutting,ball,least,attack,choice,laugh,brought,road,sudden,including,swimming,equally,smoke,total,thumb,fell,anything,bigger,west,fifteen,in,planet,walk,thy,early,hole,plant,neck,motor,there,offer,shinning,ruler,harbor,closely,brain,care,grew,somewhere,brother,step,equipment,news,setting,room,happened,busy,grandmother,climb,bowl,industrial,share,arrangement,allow,beat,variety,thin,however,whom,send,escape,piece,whether,situation,dark,pig,frog,nodded,actually,sort,anyway,tie,consider,spin,unless,product,trail,rock,table,stems,rhyme,soon,bottom,throughout,ordinary,simple,therefore,angry,floor,positive,burn,pale,food,importance,more,belong,industry,pool,only,trap,chain,easily,heard,rather,list,people,twice,winter,grade,able,poem,begun,die,thirty,storm,purpose,stock,born,silver,burn,seen,if,greatly,clean,worker,including,burst,morning,partly,teeth,whole,course,alive,experiment,bowl,perfectly,gradually,ride,story,inch,him,point,harbor,current,pain,vertical,tiny,book,act,pale,anything,per,blew,world,dozen,equally,slightly,down,electricity,when,money,contrast,rocky,below,plate,electric,animal,closely,dirt,catch,jack,outer,environment,did,straw,point,cause,hidden,especially,manner,date,ahead,die,aside,fifth,one,function,slide,cover,library,note,correct,exact,percent,bent,wheel,stuck,accept,invented,owner,broad,journey,wolf,beyond,dance,honor,end"
+// class Item {
+//     constructor(public key: string, public value: any) { }
+// }
+// class LinkedListItem {
+//     value: Item;
+//     next: LinkedListItem;
+//     push(){}
+//     find(key){}
+// }
+// class Hashtabel {
+//     arr: LinkedListItem[] = [];
+//     private hash(key) {
+//         var letter = key.split('');
+//         var sum = 0;
+//         for(var j = 0; j < letter.length; j++){
+//            var number =  letter[j].charCodeAt();
+//            sum = sum + number;
+//         }
+//          return Math.ceil(sum % 50);
+//     }
+//     log() {
+//         for (var i = 0; i < this.arr.length; i++) {
+//             let bucket = this.arr[i];
+//             if (bucket) {
+//                 console.log("bucket" + i + ": " + bucket.length)
+//             } else {
+//                 console.log("bucket" + i + ": " + 0)
+//             }
+//         }
+//     }
+//     add(key, value: string) {
+//         var ind = this.hash(key);
+//         if (this.arr[ind] == undefined) {
+//             var item = new Item(key, value);
+//             var linkedListNode = new LinkedListItem();
+//             linkedListNode.value = item;
+//             this.arr[ind] = linkedListNode;
+//             return;
+//         }
+//         var exstingItem = this.arr[ind].find(item => item.key == key)
+//         if (exstingItem) {
+//             exstingItem.value = value;
+//         } else {
+//             this.arr[ind].push(new Item(key, value));
+//         }
+//     }
+//     get(key) {
+//         var ind = this.hash(key);
+//         var item = this.arr[ind].find(item => item.key == key);
+//         if (item) {
+//             return item.value;
+//         }
+//         else {
+//             return null;
+//         }
+//     }
+//     delete(key) {
+//         var ind = this.hash(key);
+//         var item = this.arr[ind].find(item => item.key == key);
+//         if (item) {
+//             var indez = this.arr[ind].indexOf(item);
+//             this.arr[ind].splice(indez, 1);
+//         }
+//     }
+// }
+// let arr2 = new Hashtabel();
+// var words =  line.split(",");
+// words.forEach(word => arr2.add(word, word));
+// arr2.add("rtesdghh", "rtesdghh");
+// var val = arr2.get("rtesdghh");
+// arr2.delete("dinner");
+// var val2 = arr2.get("dinner");
+// arr2.log();
+var ListNode = /** @class */ (function () {
+    function ListNode(value) {
         this.value = value;
+        this.next = null;
     }
-    return Item;
+    return ListNode;
 }());
-var arr = [];
-var Hashtabel = /** @class */ (function () {
-    function Hashtabel() {
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.head = null;
     }
-    Hashtabel.prototype.add = function (key, value) {
-        var ind = key.length;
-        var ar = [];
-        arr[ind] = ar;
-        ar.push(new Item(key, value));
+    LinkedList.prototype.add = function (value) {
+        var node = new ListNode(value);
+        var currentnode;
+        if (this.head === null) {
+            this.head = node;
+        }
+        else {
+            currentnode = this.head;
+            while (currentnode.next) {
+                currentnode = currentnode.next;
+            }
+            currentnode.next = node;
+        }
     };
-    Hashtabel.prototype.get = function (key) {
-        var ind = key.length;
-        var ar = [];
-        arr[ind] = ar;
-        return ar[key];
-    };
-    Hashtabel.prototype["delete"] = function (key) {
-        var ind = key.length;
-        var ar = [];
-        arr[ind] = ar;
-        delete ar[key];
-    };
-    return Hashtabel;
+    return LinkedList;
 }());
-var arr2 = new Hashtabel();
-arr2.add("assdfff", 67);
-arr2.add("as4dfff", 57);
-arr2.add("assdfff", 47);
-arr2.get("assdfff");
-console.log("assdfff");
-console.log(arr);
+var ll = new LinkedList();
+ll.add("Hello World");
+ll.add(8);
+ll.add("rrrrr");
+console.log(ll); // <- это что???
